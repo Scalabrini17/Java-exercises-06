4408398d5e68c725e924f9b0ceda406602338f1f
Scalabrini17 <itallomartins2007@gmail.com>
2026-04-01 14:17:30 -0300

    update commit

---
+++ src/EX5.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class EX5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Olá, bem vindo a Seu Perfil de Vida!");
+
+        System.out.println("Para começar digite o seu nome: ");
+        String nome = sc.next();
+        
+
+    }
+}
